f0afd26c170d55e377cbe60021b04f0909f20b16
unknown <giabbanienzo@gmail.com>
2024-12-03 15:07:14 +0100

    Ajout d'une fenêtre

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
-liste = list(range(1,11))
-for i in liste:
-    print(i)
+import pygame
+from pygame.locals import *
+from random import randint
+
+pygame.init()
+LARGEUR = 600
+HAUTEUR = 600
+fenetre = pygame.display.set_mode((LARGEUR, HAUTEUR))
+clock = pygame.time.Clock()
+continuer = True
+
+
+while continuer:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            continuer = False
+
+
+    pygame.display.flip()
+    fenetre.fill((0, 0, 0))
+    clock.tick(60)
+
+pygame.quit()
