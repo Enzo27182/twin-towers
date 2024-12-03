9e07986be074391f334720db24079f444b748899
unknown <giabbanienzo@gmail.com>
2024-12-03 16:14:55 +0100

    affichage fenetre

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from pygame.locals import *
 
 # initialisation de pygame
 pygame.init()
-fenetre = pygame.display.set_mode((800, 600), FULLSCREEN)
+fenetre = pygame.display.set_mode((800, 600))
 clock = pygame.time.Clock()
 listes_des_srpites = pygame.sprite.LayeredUpdates()
 
@@ -13,3 +13,18 @@ class Avion(pygame.sprite.Sprite):
         super().__init__()
         self.image = pygame.image.load("avion.png").convert_alpha()
         self.rect = self.image.get_rect()
+
+listes_des_srpites.draw(fenetre)
+pygame.display.flip()
+pygame.key.set_repeat(1,0)
+
+continuer = True
+while continuer:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            continuer = False
+
+
+    listes_des_srpites.draw(fenetre)
+    pygame.display.flip()
+pygame.quit()
