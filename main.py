f8b80ef9951f3e39109c775457d6dab9b7de547f
evona8 <marine.grigorov@yahoo.com>
2024-12-03 15:56:59 +0100

    ajout_deux_images

--- main.py
+++ main.py
@@ -1,27 +1,9 @@
 
 import pygame
 from pygame.locals import *
-from random import randint
 
 pygame.init()
-LARGEUR = 600
-HAUTEUR = 600
-fenetre = pygame.display.set_mode((LARGEUR, HAUTEUR))
-clock = pygame.time.Clock()
-continuer = True
 
 
-while continuer:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            continuer = False
 
-
-    pygame.display.flip()
-    fenetre.fill((0, 0, 0))
-    clock.tick(60)
-
-pygame.quit()
-
-print("Hello world !")
 
