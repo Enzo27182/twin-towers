829557e645e1e36d5c4f1eeb98f6ac7d6021c01e
unknown <giabbanienzo@gmail.com>
2024-12-17 16:38:11 +0100

    modification truc

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import pygame
+from pygame.key import get_mods
 from pygame.locals import *
 
 # initialisation de pygame
 pygame.init()
-fenetre = pygame.display.set_mode((800, 600))
+fenetre = pygame.display.set_mode((800, 600),FULLSCREEN)
 clock = pygame.time.Clock()
-listes_des_srpites = pygame.sprite.LayeredUpdates()
+liste_des_sprites = pygame.sprite.LayeredUpdates()
+liste_batiments = pygame.sprite.LayeredUpdates()
 
 # classes
 class Fond(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
         self.image = pygame.image.load("fond.png").convert_alpha()
+        self.image = pygame.transform.scale_by(self.image, 0.5)
         self.rect = self.image.get_rect()
         self.rect.x = 0
         self.rect.y = 0
@@ -23,6 +26,14 @@ class Avion(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = 100
         self.rect.centery = fenetre.get_rect().centery
+        self.vitesse = 0
+
+    def voler(self):
+        self.rect.y += self.vitesse
+        self.vitesse += 0.1
+
+    def monter(self):
+        self.vitesse = -5
 
 class Batiment(pygame.sprite.Sprite):
     def __init__(self):
@@ -38,12 +49,12 @@ avion = Avion()
 batiment = Batiment()
 
 # ajout des sprites a la liste
-listes_des_srpites.add(fond)
-listes_des_srpites.add(avion)
-listes_des_srpites.add(batiment)
+liste_des_sprites.add(fond)
+liste_des_sprites.add(avion)
+liste_des_sprites.add(batiment)
 
 # affichage fenetre
-listes_des_srpites.draw(fenetre)
+liste_des_sprites.draw(fenetre)
 pygame.display.flip()
 pygame.key.set_repeat(1,0)
 
@@ -52,9 +63,24 @@ while continuer:
     for event in pygame.event.get():
         if event.type == QUIT:
             continuer = False
+        if event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                continuer = False
+            if event.key == K_SPACE:
+                avion.monter()
+
+    avion.voler()
 
     # affichage fenetre
-    listes_des_srpites.draw(fenetre)
+    liste_des_sprites.draw(fenetre)
     pygame.display.flip()
+    clock.tick(60)
+    if avion.rect.bottom >= fenetre.get_rect().bottom:
+        continuer = False
+quitter = False
+while not quitter:
+    for event in pygame.event.get():
+        if event.type == KEYDOWN and event.key == K_ESCAPE:
+            quitter = True
 pygame.quit()
 
